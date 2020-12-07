8d4c9399da81f55c2e8e6968e5f4b77d84de3f57
shouma0407 <shouma20000407@gmail.com>
2020-12-07 19:21:21 +0900

    演算子オーバーロードのテスト追加

---
+++ SortUnitTest/NumberComparisonOperator.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortUnitTest
+{
+    [TestClass]
+    public class NumberComparisonOperator
+    {
+
+        // lhs == rhs
+        [TestMethod]
+        public void OrMore_1()
+        {
+            //Arenge
+            Sort.Number number = new Sort.Number(1);
+
+            //Act
+            bool result = (number <= new Sort.Number(1));
+            
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        //lhs <= rhs
+        [TestMethod]
+        public void OrMore_2()
+        {
+            //Arenge
+            Sort.Number number = new Sort.Number(1);
+
+            //Act
+            bool result = (number <= new Sort.Number(2));
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        //lhs >= rhs
+        [TestMethod]
+        public void OrMore_3()
+        {
+            //Arenge
+            Sort.Number number = new Sort.Number(2);
+
+            //Act
+            bool result = (number <= new Sort.Number(1));
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        //lhs == rhs
+        [TestMethod]
+        public void OrLess_1()
+        {
+            //Arenge
+            Sort.Number number = new Sort.Number(1);
+
+            //Act
+            bool result = (number >= new Sort.Number(1));
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        //lhs <= rhs
+        [TestMethod]
+        public void OrLess_2()
+        {
+            //Arenge
+            Sort.Number number = new Sort.Number(1);
+
+            //Act
+            bool result = (number >= new Sort.Number(2));
+
+            //Assert
+            Assert.IsFalse(result);
+        }
+
+        //lhs >= rhs
+        [TestMethod]
+        public void OrLess_3()
+        {
+            //Arenge
+            Sort.Number number = new Sort.Number(2);
+
+            //Act
+            bool result = (number >= new Sort.Number(1));
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
